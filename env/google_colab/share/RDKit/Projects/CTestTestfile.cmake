c9d1460753a678354860c0fa7bdffdb608842ec5
Finlay MacLean <finlay.maclean@benevolent.ai>
2021-01-16 12:08:04 +0000

    add env

---
+++ env/google_colab/share/RDKit/Projects/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/glandrum/anaconda3/conda-bld/rdkit_1603167990487/work/Projects
+# Build directory: /Users/glandrum/anaconda3/conda-bld/rdkit_1603167990487/work/Projects
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(pythonTestDbCLI "/Users/fmaclean/Documents/google_colab/env/google_colab/bin/python" "/Users/glandrum/anaconda3/conda-bld/rdkit_1603167990487/work/Projects/test_list.py" "--testDir" "/Users/glandrum/anaconda3/conda-bld/rdkit_1603167990487/work/Projects")
+set_tests_properties(pythonTestDbCLI PROPERTIES  _BACKTRACE_TRIPLES "/Users/glandrum/anaconda3/conda-bld/rdkit_1603167990487/work/Code/cmake/Modules/RDKitUtils.cmake;178;add_test;/Users/glandrum/anaconda3/conda-bld/rdkit_1603167990487/work/Projects/CMakeLists.txt;1;add_pytest;/Users/glandrum/anaconda3/conda-bld/rdkit_1603167990487/work/Projects/CMakeLists.txt;0;")
